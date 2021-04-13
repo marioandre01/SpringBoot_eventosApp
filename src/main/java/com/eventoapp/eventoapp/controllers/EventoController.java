affe591e1a3ab3c84d6f9d9570cac07d96a0ff12
Mário André <mario_andre.l.a@hotmail.com>
2021-04-13 09:12:22 -0300

    definido buscas no banco de dados MySQL, mostra eventos cadastrados

--- src/main/java/com/eventoapp/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/eventoapp/controllers/EventoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventoController {
@@ -25,5 +26,14 @@ public class EventoController {
         eventoRepository.save(evento);
 
         return "redirect:/cadastrarEvento";
+    }
+
+    @RequestMapping("/eventos")
+    public ModelAndView lsitaEventos() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        Iterable<Evento> eventos = eventoRepository.findAll();
+        modelAndView.addObject("eventos", eventos);
+
+        return modelAndView;
     }
 }
